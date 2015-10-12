8ae471f3b033d808ce96bd43932a48154cac4395
ErwanHaquet <ehaquet.dev@gmail.com>
2015-10-12 13:30:17 +0200

    ajout des entitées Message & RelationUserMessage

---
+++ src/CryptYO/HomeBundle/Controller/MessageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CryptYO\HomeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MessageController extends Controller
+{
+    public function sendAction()
+    {
+        return $this->render('CryptYOHomeBundle:Home:send_message.html.twig');
+        
+    }
+}
