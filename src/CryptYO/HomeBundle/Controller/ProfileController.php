ccb3da69bbc7bb795068b2e4714061783506335e
ErwanHaquet <ehaquet.dev@gmail.com>
2015-10-14 14:38:15 +0200

    ..

---
+++ src/CryptYO/HomeBundle/Controller/ProfileController.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CryptYO\HomeBundle\Controller;
+
+use FOS\UserBundle\Controller\ProfileController as BaseController;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use CryptYO\HomeBundle\Entity\Message;
+
+/**
+ * Controller managing the user profile
+ *
+ * @author Christophe Coevoet <stof@notk.org>
+ */
+class ProfileController extends BaseController
+{
+    public function showAction()
+    {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->render('FOSUserBundle:Profile:show.html.twig', array(
+            'user' => $user,
+        ));
+    }
+
+
+
+}
